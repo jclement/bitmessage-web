2ce2f3ce32e1587af451b5f4c6fcc4dcdf20d4f0
Jeff Clement <jeffrey.clement@gmail.com>
2014-11-08 21:04:10 -0700

    adding avatars

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ var path = require('path');
 var logger = require('morgan');
 var fs = require('fs');
 var _ = require('underscore');
+var crypto = require('crypto');
+var uuid = require('node-uuid');
 
 var session = require('./lib/session');
 var config = require('./lib/config');
@@ -23,6 +25,45 @@ app.use("/lib", express.static(path.join(__dirname, '..', 'bower_components')));
 app.use("/fonts", express.static(path.join(__dirname, '..', 'bower_components', 'bootstrap', 'fonts')));
 app.use('/api/auth', route_authentication);
 app.use('/api/bm', route_bm);
+app.use('/api/avatar', function (req, res) {
+    // avatars are pulled from robohash.org
+    // string passed to robohash is a random UUID and has no relation to the address being pulled.
+    // the image is saved locally against the address to limit risks of robohash.org being monitored
+
+    // filename for local cache is sha1 of path.  This eliminates need for special character testing.
+    var sha = crypto.createHash('sha1');
+    sha.update(req.path);
+    var filename = sha.digest('hex') + '.png';
+
+    // full path for image file
+    var fullPath = path.join(__dirname, '..', 'cache', 'avatars', filename)
+
+    fs.exists(fullPath, function (exists) {
+        res.writeHead(200, {'Content-type': 'image/png'});
+        if (exists) {
+            // if image already exists, stream it
+            fs.createReadStream(fullPath).pipe(res);
+        } else {
+            // generate UUID for robohash request, pull down image and save it
+            var randomString = uuid.v4();
+            var file = fs.createWriteStream(fullPath);
+            http.get('http://robohash.org/' + randomString + '.png?size=24x24', function (imageResponse) {
+                imageResponse.on('data', function (data) {
+                    file.write(data);
+                    res.write(data);
+                }).on('end', function () {
+                    file.end();
+                    res.end();
+                }).on('error', function(e) {
+                    console.log(e);
+                    file.end();
+                    fs.unlink(filePath);
+                });
+            });
+        }
+    });
+})
+;
 
 var port = config('port') || 3000;
 http.createServer(app).listen(port, function () {
